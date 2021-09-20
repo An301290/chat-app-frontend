0d41fc34b6659e88a1397f5131a2a775f3e2fae5
Angel <ansahamano@gmail.com>
2021-09-20 12:27:40 +0200

    Fixed bugs within signup

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -22,39 +22,37 @@ const Auth = () => {
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
-  const switchMode = () => {
+  
+   const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const { username, password, phoneNumber, avatarURL } = form;
+
+        const URL = 'https://localhost:5000/auth';
+
+        const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName: form.fullName, phoneNumber, avatarURL,
+        });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
+    }
+
+
+    const switchMode = () => {
     setIsSignup((prevIsSignup) => !prevIsSignup);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const { username, password, phoneNumber, avatarURL } = form;
-
-    const URL = "https://localhost:5000/auth";
-
-    const {
-      data: { token, userId, hashedPassword, fullName },
-    } = await axios.post(`${URL}/${isSignup ? "signup" : "login"}`, {
-      username,
-      password,
-      fullName: form.fullName,
-      phoneNumber,
-      avatarURL,
-    });
-
-    cookies.set("token", token);
-    cookies.set("username", username);
-    cookies.set("fullName", fullName);
-    cookies.set("userId", userId);
-
-    if (isSignup) {
-      cookies.set("phoneNumber", phoneNumber);
-      cookies.set("avatarURL", avatarURL);
-      cookies.set("hashedPassword", hashedPassword);
-    }
-    window.location.reload();
-  };
 
   return (
     <div className="auth__form-container">
